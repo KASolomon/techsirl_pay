ada7d50c82326f344d545badbe2a0065e175e8b8
KASolomon <kusiappiahsolomon10@gmail.com>
2024-11-01 23:29:09 +0000

    build user info form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,160 @@
-import Image from "next/image";
+"use client";
+import { z } from "zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const schema = z.object({
+    fullName: z.string().min(3, { message: "Kindly enter your name." }),
+    // lastName: z.string().min(3),
+    amount: z.string().min(1, { message: "Please enter a correct amount." }),
+    email: z
+      .string({ message: "Please provide a valid email." })
+      .email({ message: "Please provide a valid email." }),
+    phone: z
+      .string({ message: "Please enter your phone number." })
+      .min(10, { message: "Please provide a valid phone number." })
+      .max(15, { message: "Please provide a valid phone number." }),
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      fullName: "",
+      amount: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const handlePayment = (userInfo: z.infer<typeof schema>) => {
+    console.log(userInfo);
+  };
+
+  return (
+    <div className=" bg-neutral-50 dark:bg-blue-800 min-h-screen w-full flex items-center justify-center">
+      <div className=" py-8 px-4 md:p-8 my-10  flex flex-col justify-center  items-center rounded-xl shadow-lg shadow-gray-600 dark:bg-blue-950 w-[90vw] md:w-[60vw] min-h-[500px] ">
+        <h3 className="text-3xl text-center text-emerald-700 font-parisFont">
+          Techsirl Photography
+        </h3>
+        <h3 className="mt-2 text-sm text-center ">Payment page</h3>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handlePayment)}
+            className="space-y-10 my-6 min-w-[250px]"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            {/* <div className="grid md:grid-cols-[1fr_1fr] gap-4 w-full"> */}
+            <FormField
+              control={form.control}
+              name={"fullName"}
+              render={({ field: { value, onChange } }) => (
+                <FormItem>
+                  <FormLabel className="text-lg text-neutral-500">
+                    Full Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input value={value} onChange={onChange} />
+                  </FormControl>
+                  {form.formState.errors?.fullName && <FormMessage />}
+                </FormItem>
+              )}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+            {/* <FormItem>
+                <FormControl>
+                  <Input
+                   "
+                  />
+                </FormControl>
+              </FormItem> */}
+            {/* </div> */}
+
+            <FormField
+              control={form.control}
+              name={"phone"}
+              render={({ field: { value, onChange } }) => (
+                <FormItem>
+                  <FormLabel className="text-lg text-neutral-500">
+                    Phone Number
+                  </FormLabel>
+                  <FormControl>
+                    <Input value={value} onChange={onChange} />
+                  </FormControl>
+                  {form.formState.errors?.phone && <FormMessage />}
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name={"email"}
+              render={({ field: { value, onChange } }) => (
+                <FormItem>
+                  <FormLabel className="text-lg text-neutral-500">
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input value={value} onChange={onChange} />
+                  </FormControl>
+                  {form.formState.errors?.email && <FormMessage />}
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name={"amount"}
+              render={({ field: { value, onChange } }) => (
+                <FormItem>
+                  <FormLabel className="text-lg text-neutral-500">
+                    Amount
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min={1}
+                      value={value}
+                      onChange={onChange}
+                    />
+                  </FormControl>
+                  {form.formState.errors?.amount && <FormMessage />}
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center justify-center w-full">
+              <FormItem>
+                <FormControl>
+                  <Button
+                    size={"lg"}
+                    className="bg-blue-800 text-lg"
+                    type="submit"
+                  >
+                    Start Payment
+                  </Button>
+                </FormControl>
+              </FormItem>
+            </div>
+          </form>
+        </Form>
+        <h6 className="text-center text-slate-600">
+          All payments are secured with{" "}
+          <Link href="https://paystack.com/" className="text-blue-700">
+            Paystack
+          </Link>
+        </h6>
+      </div>
     </div>
   );
 }
